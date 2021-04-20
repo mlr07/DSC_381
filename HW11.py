20853f1ca4e5c5ee4e3cbca796e275ab7fac379e
mrhoads <mrhoads1@gmail.com>
2021-04-19 21:11:42 -0500

    more HW11 hacking

--- HW11.py
+++ HW11.py
@@ -11,6 +11,7 @@ from IPython.display import display
 sns.set(rc={'figure.figsize':(12,10)})
 
 # %%
+# 1
 # curl -o MustangPrice.csv https://www.lock5stat.com/datasets3e/MustangPrice.csv
 # 90% CI for single population mean price --> test for conditions
 
@@ -33,6 +34,7 @@ plt.show()
 
 
 # %%
+# 2
 # curl -o CommuteAtlanta.csv https://www.lock5stat.com/datasets3e/CommuteAtlanta.csv
 # HT on single mean: Ho = 30min, Ha < 30min
 
@@ -60,23 +62,22 @@ col = "Time"
 single_mean_conditions(data, col)
 
 # %%
-df = pd.read_csv("./data/ICUAdmissions.csv")
-
+# 3
 # 90% CI for difference in two proportions  --> meet conditions?
 # proportion of non cancer with infections = non cancer infection/non-cancer
 # proportion of cancer with infection = cancer infection/cancer
+
+df = pd.read_csv("./data/ICUAdmissions.csv")
 
 no_cancer = df[df["Cancer"] == 0]
 no_cancer_infect = no_cancer["Infection"] == 1
 prop_no_cancer_infect = sum(no_cancer_infect) / len(no_cancer)
 print(f"No cancer infected: prop = {prop_no_cancer_infect:.2f}, c = {sum(no_cancer_infect)}, n = {len(no_cancer)}")
 
-
 cancer = df[df["Cancer"]== 1]
 cancer_infect = cancer["Infection"] == 1
 prop_cancer_infect = sum(cancer_infect) / len(cancer)
 print(f"Cancer infected: prop = {prop_cancer_infect:.2f}, c = {sum(cancer_infect)}, n = {len(cancer)}")
-
 
 # TODO: make verbose with p1, p2, n1, n2
 if (prop_no_cancer_infect*len(no_cancer) >= 10) and ((1-prop_no_cancer_infect)*len(no_cancer) >= 10):
@@ -88,6 +89,7 @@ else:
     print("Neither proprotions satisfy conditions.")
 
 # %%
+# 4,5,6
 
 def single_prop_condition(sample, count, q, p0=None):
     if p0:
@@ -122,6 +124,7 @@ sample, defect = 200, 12
 single_prop_condition(sample=sample, count=defect, q="6", p0=p0)
 
 # %%
+# 7
 # curl -o ExerciseHours.csv https://www.lock5stat.com/datasets3e/ExerciseHours.csv
 # HT for difference of means: Ho: mean_m == mean_f, Ha: mean_m != mean_f
 # condition for t-dist: each group n >= 30
@@ -140,6 +143,7 @@ else:
     print("Conditions for difference of means NOT MET")
 
 # %%
+# 8
 # curl -o TrafficFlow.csv https://www.lock5stat.com/datasets3e/TrafficFlow.csv
 # difference of means with paired data
 # condition for t-dist: clt or n >= 30 
@@ -161,6 +165,7 @@ plt.ylabel("Count", labelpad=12)
 plt.show()
 
 # %%
+# 9
 # test for association between categories
 # condition for chi**2: each category expected >= 5 
 # from statkey --> there is an equation
@@ -181,6 +186,7 @@ else:
     print("Conditions for Chi**2 association test NOT MET")
 
 # %%
+# 10
 # curl -o RestaurantTips.csv https://www.lock5stat.com/datasets3e/RestaurantTips.csv
 # test for a difference of means between groups ANOVA
 # condition: clt n >= 30 for each group and SDmax / SDmin < 2
@@ -195,22 +201,81 @@ for k,v in server_counts.items():
         res.append(True)
     else:
         res.append(False)
-
-if all(res) == True:
-    print("Count conditions for ANOVA MET")
-else:
-    print("Count conditions for ANOVA NOT MET")
-
-pct_tip_std = {}
-for k,v in server_counts.items():
     server = df[df["Server"] == k]
     pct_tip_std[k] = server["PctTip"].std()
 
 min_std = min(pct_tip_std.values())
 max_std = max(pct_tip_std.values())
 
+if all(res) == True:
+    print("Count conditions for ANOVA MET")
+else:
+    print("Count conditions for ANOVA NOT MET")
+
 if max_std / min_std < 2.0:
     print("STD conditions for ANOVE MET")
 else:
     print("STD conditions for ANOVA NOT MET")
+
 # %%
+# 11
+# curl -o FootballBrain.csv https://www.lock5stat.com/datasets3e/FootballBrain.csv
+# test for difference of mean between groups ANOVA
+# condition: clt n >= 30 for each group and SDmax / SDmin < 2
+# Ho: mean1 == mean2 == mean3, Ha: one pair of means !=
+# comparative box plots
+# ANOVA for diff means: F-stat = 2.651, n = 44, df: groups = 1, error = 42, total = 43
+# 1-tail test: pval = 0.115, alpha = 0.05 --> do not reject null
+# Difference of means: x1 - x2 = 13.05
+# 2-tail test: pval = 0.055, alpha = 0.05 --> do not reject null
+
+df = pd.read_csv("./data/FootballBrain.csv")
+display(df.info())
+
+# %%
+sns.catplot(x="Group", y="Hipp", kind="violin", inner="stick", data=df)
+
+
+# %%
+sns.catplot(x="Group", y="Cogniton", kind="violin", inner="stick", data=df)
+
+# %%
+cognit = df[df['Cogniton'].notnull()] 
+cognit_counts = dict(cognit["Group"].value_counts())
+
+res = []
+cognit_std = {}
+for k,v in cognit_counts.items():
+    if v >= 30:
+        res.append(True)
+    else:
+        res.append(False)
+    _ = cognit[cognit["Group"] == k]
+    hipp_std[k] = _["Cogniton"].std()
+
+min_std = min(hipp_std.values())
+max_std = max(hipp_std.values())
+condit_std = max_std / min_std 
+
+if all(res) == True:
+    print("Count conditions for ANOVA MET")
+else:
+    print("Count conditions for ANOVA NOT MET")
+
+if condit_std < 2.0:
+    print("STD conditions for ANOVA MET")
+else:
+    print("STD conditions for ANOVA NOT MET")
+
+
+# %%
+# 12
+
+# %%
+# 13
+# Association between categories --> chi**2
+# condition for chi**2: each category expected >= 5 
+# chi**2-stat = 8.592
+# right 1-tail test: pval = 0.066, alpha = 0.10
+# expected counts all >= 5 except Thursday
+
