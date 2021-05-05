e8da8d4eccd9515ada6f9a1fb537a8a8361e4bd8
mtt rhoads <mrhoads1@gmail.com>
2021-05-05 10:10:35 -0500

    formatted M2 review

---
+++ M2_review.py
@@ -0,0 +1,13 @@
+# prob portion
+# MGF --> HW6
+# chernoff bound --> HW6
+# joint distribution --> HW7
+# CLT --> HW8
+
+# stats portion
+# inference --> HW4
+# simulation --> HW9
+# theoretical distn --> HW10
+# chi**2 and ANOVA --> HW11
+# MLE --> HW12
+# exp families --> HW13
